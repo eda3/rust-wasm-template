46e1426ad1beac10114d9d63493b731284977715
eda3 <eda3@users.noreply.github.com>
2021-04-19 12:09:51 +0000

    deploy: 5c003417028574c48185d5470e3af821bb7495d3

--- www/dist/bootstrap.js
+++ www/dist/bootstrap.js
@@ -159,7 +159,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rust_wasm_template_bg.wasm":"3c9d2cf74950ed48a1d8"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rust_wasm_template_bg.wasm":"748f64825d9c2b2b2431"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
